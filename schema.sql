f0ca287fb61f3ca382cd684f4c48b95b9b23f391
Emily  Beutler <51429065+embeutler@users.noreply.github.com>
2019-11-26 03:06:25 -0800

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT(10) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT (100) NOT NULL,
+  PRIMARY KEY (item_id)
+);
